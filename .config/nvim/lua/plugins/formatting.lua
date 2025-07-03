5cb0e3ff0fcad89077c68f2386610aba3570fc90
jacocanete <jacogagarincanete@gmail.com>
2025-07-04 07:05:44 +0800

    feat: add undotree and flash plugins, improve completion sources, and clean configs

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,36 +1,36 @@
 -- Autoformat
-local tools = require("config.tools")
+local tools = require "config.tools"
 
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				return nil
-			else
-				return {
-					timeout_ms = 1000,
-					lsp_format = "fallback",
-				}
-			end
-		end,
-		formatters_by_ft = tools.formatters or {},
-	},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format { async = true, lsp_format = "fallback" }
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 1000,
+          lsp_format = "fallback",
+        }
+      end
+    end,
+    formatters_by_ft = tools.formatters or {},
+  },
 }
